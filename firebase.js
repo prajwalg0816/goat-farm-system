22da7cf2740a4a001259fce0670175330074539b
Prajwal g <98144541+prajwalg0816@users.noreply.github.com>
2026-01-14 11:54:48 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,10 @@
-var firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID"
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCOSeK0RonTva_lazfZV_H22sFAzd685iY",
+  authDomain: "goatfarmmanagement-d7b89.firebaseapp.com",
+  projectId: "goatfarmmanagement-d7b89",
+  storageBucket: "goatfarmmanagement-d7b89.firebasestorage.app",
+  messagingSenderId: "141604591730",
+  appId: "1:141604591730:web:8736d4cfae6abfa6ebcaeb",
+  measurementId: "G-B1YCPQ7ZRW"
 };
-firebase.initializeApp(firebaseConfig);
