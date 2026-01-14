e1ef3ce6860da11dab19807d48df78609dca1798
Prajwal g <98144541+prajwalg0816@users.noreply.github.com>
2026-01-14 12:08:21 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,13 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+// firebase.js (COMPAT VERSION)
+
+var firebaseConfig = {
   apiKey: "AIzaSyCOSeK0RonTva_lazfZV_H22sFAzd685iY",
   authDomain: "goatfarmmanagement-d7b89.firebaseapp.com",
   projectId: "goatfarmmanagement-d7b89",
-  storageBucket: "goatfarmmanagement-d7b89.firebasestorage.app",
+  storageBucket: "goatfarmmanagement-d7b89.appspot.com",
   messagingSenderId: "141604591730",
-  appId: "1:141604591730:web:8736d4cfae6abfa6ebcaeb",
-  measurementId: "G-B1YCPQ7ZRW"
+  appId: "1:141604591730:web:8736d4cfae6abfa6ebcaeb"
 };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
